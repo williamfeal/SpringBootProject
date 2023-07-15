3a4a447db416350fb3166da7910e3b819a4a0c91
William Feal <william.feal@gmail.com>
2023-07-15 14:05:20 +0200

    (fix): delete folder microservices

--- microservices/src/main/java/com/myspringproject/microservices/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.myspringproject.microservices.repository;
-
-import com.myspringproject.microservices.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
