e787a7bf17b723790d168e8a9ee5a07d3fc7e2f1
williamfeal <william.feal@gmail.com>
2023-07-19 01:09:21 +0200

    (fix): jwt change to test password

--- auth-service/src/main/java/com/myspringproject/authservice/AuthenticationServiceApplication.java
+++ auth-service/src/main/java/com/myspringproject/authservice/AuthenticationServiceApplication.java
@@ -3,6 +3,8 @@ package com.myspringproject.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +12,19 @@ public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServiceApplication.class, args);
+
+		// Codificar la contraseña
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "15103028";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+
+		System.out.println("Encoded password: " + encodedPassword);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
+
+
--- auth-service/src/main/java/com/myspringproject/authservice/service/MyUserDetailsService.java
+++ auth-service/src/main/java/com/myspringproject/authservice/service/MyUserDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,9 @@ public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
@@ -24,5 +28,7 @@ public class MyUserDetailsService implements UserDetailsService {
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
     }
+
+
 }
 
